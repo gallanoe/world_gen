fdd7db60d8e9452a46ba8a3ac3482c4ff97def7a
gallanoe <gallanoeero@gmail.com>
2019-01-06 00:40:53 -0800

    reorganized code and centralized tests and timing

--- test.py
+++ test.py
@@ -1,64 +1,61 @@
-import math
-import random
-import copy
+import networkx_model as nx_model
+import primitives_model as pr_model
+import timeit
 
-import helper
+def networkx_test():
+    def generate_world(n):
+        w = nx_model.World(npoints=n)
+        w.generate()
+        w.plot()
 
-import numpy as np
+    def gen500():
+        generate_world(500)
 
-import matplotlib.pyplot as plt
-import matplotlib.tri as mtri
-from matplotlib import collections, colors
-from mpl_toolkits import mplot3d
-from mpl_toolkits.mplot3d import axes3d, proj3d
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
-from scipy.spatial import ConvexHull, SphericalVoronoi
-from collections import deque, namedtuple
+    def gen1000():
+        generate_world(1000)
 
-radius = 4000
-npoints = 10
+    def gen2000():
+        generate_world(2000)
 
-# create point
-phi = np.linspace(0, np.pi, 20)
-theta = np.linspace(0, 2 * np.pi, 40)
-grid_x = np.outer(np.sin(theta) * np.sqrt(radius * 1.001), np.cos(phi) * np.sqrt((radius * 1.001)))
-grid_y = np.outer(np.sin(theta) * np.sqrt(radius * 1.001), np.sin(phi) * np.sqrt((radius * 1.001)))
-grid_z = np.outer(np.cos(theta) * np.sqrt(radius * 1.001), np.ones_like(phi) * np.sqrt((radius * 1.001)))
+    def gen5000():
+        generate_world(5000)
 
-# generate random point
-points = np.random.randn(3, npoints)
-
-points /= np.linalg.norm(points, axis=0)
-points *= radius
-
-points = np.array(list(zip(points[0], points[1], points[2])))
-
-def generate_vecs(points):
-    vecs = []
-    for point in points:
-        # treat point as vector (vectors from radius are othonormal to the surface of sphere)
-        x = np.random.randn(3)
-        x -= x.dot(point) * point / np.linalg.norm(point) ** 2
-        x /= np.linalg.norm(x)
-        x *= (radius / 50)
-        vecs.append(x)
-    return np.array(vecs)
-        
-vecs = generate_vecs(points)
-print(vecs)
+    print("NetworkX on 500:", timeit.timeit(gen500, number=1))
+    print("NetworkX on 1000:", timeit.timeit(gen1000, number=1))
+    print("NetworkX on 2000:", timeit.timeit(gen2000, number=1))
+    print("NetworkX on 5000:", timeit.timeit(gen5000, number=1))
     
+def primitives_test():
+    def generate_world(n):
+        w = pr_model.World(npoints=n)
+        w.generate()
+        w.plot()
 
-fig, ax = plt.subplots(1, 1, figsize=(15, 15), subplot_kw={'projection':'3d', 'aspect':'equal'})
+    def gen500():
+        generate_world(500)
 
-# hide gridlines and labels
-ax.grid(False)
-plt.axis('off')
+    def gen1000():
+        generate_world(1000)
 
-ax.plot_wireframe(grid_x, grid_y, grid_z, alpha=0.2, color='k', rstride=1, cstride=1)
-ax.scatter(points[:,0], points[:,1], points[:,2], s=10, c='r')
-ax.quiver(points[:,0], points[:,1], points[:,2], vecs[:,0], vecs[:,1], vecs[:,2], length=3)
+    def gen2000():
+        generate_world(2000)
 
-for angle in range(360):
-    ax.view_init(30, angle)
-    plt.draw()
-    plt.pause(.001)
+    def gen5000():
+        generate_world(5000)
+
+    print("Primitives on 500:", timeit.timeit(gen500, number=1))
+    print("Primitives on 1000:", timeit.timeit(gen1000, number=1))
+    print("Primitives on 2000:", timeit.timeit(gen2000, number=1))
+    print("Primitives on 5000:", timeit.timeit(gen5000, number=1))
+
+def skip_func(functions):
+    for func in functions:
+        try:
+            func()
+        except KeyboardInterrupt:
+            print()
+            print("SKIPPING")
+            print()
+
+if __name__ == "__main__":
+    skip_func([networkx_test, primitives_test])
